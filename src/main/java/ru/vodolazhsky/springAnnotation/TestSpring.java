1f81d51f70d6dd0e7e7f0f15c41ad3e0e97d19d8
Maxim Vodolazshky <maximazerr@mail.ru>
2020-07-02 00:59:19 +0300

    Spring Annotation

--- src/main/java/ru/vodolazhsky/springAnnotation/TestSpring.java
+++ src/main/java/ru/vodolazhsky/springAnnotation/TestSpring.java
@@ -8,9 +8,8 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         MusicPlayer MusicPlayer = context.getBean("MusicPlayer", MusicPlayer.class);
-
+        System.out.println("123");
         MusicPlayer.playMusic();
-
         context.close();
     }
 }
